6302e6b53f39f1c053dffe3cd73c7f17cd96ee3b
Moritz <moritz.tanz@stud.htwk-leipzig.de>
2023-10-01 19:07:35 +0200

    fixed double instance of alpine

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         
         <script src="https://cdn.tailwindcss.com"></script>
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.0/dist/cdn.min.js"></script>
+        {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.0/dist/cdn.min.js"></script> --}}
 
     </head>
     <body class="font-sans antialiased">
