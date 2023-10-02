324199f42faabe1c4e4ee3013b5297f1213e7f43
Moritz <moritz.tanz@stud.htwk-leipzig.de>
2023-10-02 18:46:26 +0200

    changed response type of api to json globally

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,12 +17,19 @@ use Illuminate\Validation\ValidationException;
 |
 */
 
+/*  
+/   IMPORTANT NOTICE: I added a middleware globaly to the app/Http/Kernel.php to
+/   respond with JSON to all API request, ignoring their "Accept" header. 
+*/
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/test', function () {
+Route::middleware('auth:sanctum')->get('/test', function () {
     return "ok";
 });
+
+/* TODO: Refactor to own Controller? */
 Route::post(
     '/token',
     function (Request $request) {
