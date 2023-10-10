0b76f579596d29dc92f3cb9f12a78b156880ff5e
Moritz <moritz.tanz@stud.htwk-leipzig.de>
2023-10-10 20:06:27 +0200

    added build number output

---
+++ backend/config/build.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'nr' => env('BUILD_NR', '-'),
+    'date' => env('BUILD_DATE', '-'),
+];
--- backend/resources/views/dashboard.blade.php
+++ backend/resources/views/dashboard.blade.php
@@ -9,7 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <strong>E</strong>VENT - <strong>M</strong>ANAGEMENT - <strong>S</strong>YSTEM
+                    <p class="text-xs italic">by TNZ Dienstleistungen | Build @php echo config('build.nr') . " vom " . config('build.date') @endphp </p>
                 </div>
             </div>
         </div>
