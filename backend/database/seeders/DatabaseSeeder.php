4ed718c3b5cdef281409fc5f892552fd0e5dd0cb
Moritz Tanz <mtanz@stud.htwk-leipzig.de>
2023-11-08 16:02:45 +0100

    seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Event;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,18 +19,28 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
+
+        $user = \App\Models\User::factory()->create([
              'name' => 'Moritz',
              'email' => 'admin@der-taenzer.net',
              'password' => '$2y$10$bYhzpsczfNrhE6/Pt7pdKOnyLF6e15WAmXSkPIlYOJ3P7bdw/YO3u'
          ]);
 
-         $e = new \App\Models\Event();
-         $e->name = "Halloweenparty";
-         $e->date = "2023-10-30";
-         $e->time = "20:00";
-         $e->user_id = 1;
-         $e->save();
-         
+
+        Product::factory(10)->create();
+
+
+         Event::factory(3)->create([
+             "user_id" => $user->id,
+         ])->each(function ($event){
+             Order::factory(100)->create([
+                 "event_id" => $event->id,
+             ])->each(function($order){
+                OrderItem::factory(4)->create([
+                    "order_id" => $order->id
+                ]);
+             });
+         });
+
     }
 }
