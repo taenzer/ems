5c22c9458999678bb9951e0ae90aad19c876a180
Moritz <dev@der-taenzer.net>
2024-06-28 16:31:56 +0200

    added link

--- backend/resources/views/ticket/index.blade.php
+++ backend/resources/views/ticket/index.blade.php
@@ -2,14 +2,14 @@
     <x-slot name="header">
         <x-header heading="Tickets">
             <x-slot name="actions">
-                <a href=""><x-primary-button>Ticket verkaufen</x-primary-button></a>
+                <a href="{{route("tickets.orders.index")}}"><x-primary-button>Ticket verkaufen</x-primary-button></a>
                 <a href="{{ route('tickets.products.index') }}"><x-secondary-button>Ticket Produkte verwalten</x-secondary-button></a>
             </x-slot>
         </x-header>
     </x-slot>
     <x-body>
         <x-body-box>
-
+            Work in Progress!
         </x-body-box>
 
     </x-body>
