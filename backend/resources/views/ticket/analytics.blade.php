ac7b02f0f48500105227fad2a7805cfb189344fd
Moritz <moritz.tanz@stud.htwk-leipzig.de>
2024-08-28 23:54:12 +0200

    removed temp ticket bugfix

--- backend/resources/views/ticket/analytics.blade.php
+++ backend/resources/views/ticket/analytics.blade.php
@@ -14,15 +14,6 @@
             <h3 clasS="font-semibold mb-4">Umsätze</h3>
             <x-tables.ticket-sales-stats :ticketSaleStats="$ticketSaleStats" />
         </x-body-box>
-
-        <x-body-box>
-            <h3 class="font-semibold mb-4">Temporärer Bug-Workaround</h3>
-            <p class="mb-4">Auf Grund eines Fehlers in der EMS Ticketscan App werden Tickets die an der Abendkasse gekauft werden nicht automatisch eingecheckt. Da aber 
-                davon auszugehen ist, dass Menschen die an der Abendkasse ein Ticket kaufen dieses auch nutzen, können über den folgenden Button alle Tickets
-                die an der Abendkasse gekauft wurden eingecheckt werden.
-            </p>
-            <x-link-button link="{{ route('tickets.utils.boxofficeTicketCheckin', ['event' => $event]) }}">Abendkasse Ticket Checkin</x-link-button>
-        </x-body-box>
     </x-body>
     
 </x-app-layout>
