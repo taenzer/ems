d32ebb5cdab7583889e281fc85e51dedcd1dbbbc
Moritz <moritz.tanz@stud.htwk-leipzig.de>
2024-11-18 13:24:30 +0100

    changed dev server

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -4,11 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "0.0.0.0", // Erlaubt Zugriff von außerhalb des Containers
+        port: 5173, // Setzt den Port auf 5173 (oder einen anderen offenen Port)
+        origin: "http://localhost:5173", // Setzt die Basis-URL für Assets
+    },
 });
